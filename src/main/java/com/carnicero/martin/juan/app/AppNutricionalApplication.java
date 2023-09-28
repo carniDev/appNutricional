6919d5683a3340e0965fcced2b1be85843c70f3f
carnidev <munozmartinjuan@gmail.com>
2023-09-28 20:11:02 +0200

    primer commit creacion del proyecto de springboot

---
+++ src/main/java/com/carnicero/martin/juan/app/AppNutricionalApplication.java
@@ -0,0 +1,13 @@
+package com.carnicero.martin.juan.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppNutricionalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppNutricionalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carnicero/martin/juan/app/AppNutricionalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carnicero.martin.juan.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppNutricionalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
