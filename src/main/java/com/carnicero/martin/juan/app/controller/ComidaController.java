1e0293be377b5ef9f4f13e58928baf7e5af395a3
Carnidev <carniceromartinjuan@gmail.com>
2023-10-15 23:45:14 +0200

    Optimizando codigo comida

--- src/main/java/com/carnicero/martin/juan/app/controller/ComidaController.java
+++ src/main/java/com/carnicero/martin/juan/app/controller/ComidaController.java
@@ -7,16 +7,14 @@ import com.carnicero.martin.juan.app.request.RegistrarComida;
 import com.carnicero.martin.juan.app.response.InformacionComida;
 import com.carnicero.martin.juan.app.service.ComidaService;
 import com.carnicero.martin.juan.app.util.converter.InformacionComidaConverter;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import static com.carnicero.martin.juan.app.util.converter.InformacionComidaConverter.*;
 
 @RestController
 @RequestMapping("app-nutricional/comida")
@@ -31,14 +29,8 @@ public class ComidaController {
     @GetMapping("/listar-una-comida")
     public ResponseEntity listarUnaComidaUsuarioDia(@RequestParam String fecha, @RequestParam String email, @RequestParam TipoComida tipoComida) {
         try {
-            Comida comida = comidaService.listarUnaComidaUsuarioFecha(email, fecha, tipoComida);
-            InformacionComida informacionComida = InformacionComidaConverter.comidaToInformacion(comida);
-            /*ObjectMapper mapper = new ObjectMapper();
-            mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
-            mapper.registerModule(new JavaTimeModule());
-            String json = mapper.writeValueAsString(informacionComida);
-            return ResponseEntity.ok(json);
-*/
+
+            InformacionComida informacionComida = comidaToInformacion(comidaService.listarUnaComidaUsuarioFecha(email, fecha, tipoComida));
             return ResponseEntity.ok(informacionComida);
         } catch (RuntimeException e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("No se han encontrado ninguna comida");
@@ -49,9 +41,10 @@ public class ComidaController {
 
     @GetMapping("/listar-comidas")
     public ResponseEntity listarComidasUsuarioDia(@RequestParam String fecha, @RequestParam String email) {
-        List<Comida> listarComidas = comidaService.listarComidasUsuarioFecha(email, fecha);
-        List<InformacionComida>informacion = listarComidas.stream().map(InformacionComidaConverter::comidaToInformacion).collect(Collectors.toList());
-
+        List<InformacionComida> informacion = comidaService.listarComidasUsuarioFecha(email, fecha)
+                .stream()
+                .map(InformacionComidaConverter::comidaToInformacion).
+                collect(Collectors.toList());
         return ResponseEntity.ok(informacion.isEmpty() ? "No se han encontrado registros" : informacion);
     }
 
