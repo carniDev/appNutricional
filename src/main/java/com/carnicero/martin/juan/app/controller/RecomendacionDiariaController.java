ebc20499c5812bb5a6d58cb22e890c2b2c968eb5
Carnidev <carniceromartinjuan@gmail.com>
2023-10-26 17:50:33 +0200

    constantes de recomendacion diaria

--- src/main/java/com/carnicero/martin/juan/app/controller/RecomendacionDiariaController.java
+++ src/main/java/com/carnicero/martin/juan/app/controller/RecomendacionDiariaController.java
@@ -1,12 +1,15 @@
 package com.carnicero.martin.juan.app.controller;
 
 import com.carnicero.martin.juan.app.service.interfaces.RecomendacionDiariaService;
+import com.carnicero.martin.juan.app.util.Constantes.Constantes;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static com.carnicero.martin.juan.app.util.Constantes.Constantes.*;
+
 @RestController
-@RequestMapping("app-nutricional/dashboard")
+@RequestMapping(NOMBRE_APP +"/"+RECOMENDACION_CONTROLLER)
 public class RecomendacionDiariaController {
 
 
@@ -16,7 +19,7 @@ private final RecomendacionDiariaService recomendacionService;
         this.recomendacionService = recomendacionService;
     }
 
-    @GetMapping("informacion-diaria")
+    @GetMapping(INFORMACION_DIARIA)
     public ResponseEntity obtenerInformacionDiaria(@RequestParam String fechaDia, @RequestParam String email){
         try{
             return ResponseEntity.ok(recomendacionService.obtenerInformacion(fechaDia,email));
--- src/main/java/com/carnicero/martin/juan/app/util/Constantes/Constantes.java
+++ src/main/java/com/carnicero/martin/juan/app/util/Constantes/Constantes.java
@@ -4,6 +4,8 @@ public final class Constantes {
     public final static String NOMBRE_APP="app-nutricional";
     public final static String ALIMENTO_CONTROLLER="alimento";
     public final static String USUARIO_CONTROLLER="usuario";
+    public final static String RECOMENDACION_CONTROLLER="dashboard";
+    public final static String INFORMACION_DIARIA="informacion_diaria";
     public final static String REGISTRAR_OK="Se ha registrado correctamente";
     public final static String EDITAR_OK="Se ha editado correctamente";
     public final static String ELIMINAR_OK ="Se ha eliminado correctamente";
